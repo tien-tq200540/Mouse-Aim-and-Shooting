83bfd15caf4000925aef09886149965bf3a92d63
tien-tq200540 <tqtien157202@gmail.com>
2026-08-27 22:05:30 +0700

    Smoothing the LookAtMouse feature with two different solution

--- Assets/Scripts/Player/LookAtMouse.cs
+++ Assets/Scripts/Player/LookAtMouse.cs
@@ -5,8 +5,9 @@ using UnityEngine.InputSystem;
 
 public class LookAtMouse : MonoBehaviour
 {
-    [SerializeField] private Vector2 mouseScreenPos;
-    [SerializeField] private Vector2 mouseWordPos;
+    [SerializeField] private Vector3 mouseScreenPos;
+    [SerializeField] private Vector3 mouseWordPos;
+    [SerializeField] private float rotSpeed = 720f;
 
     private void Update()
     {
@@ -22,10 +23,40 @@ public class LookAtMouse : MonoBehaviour
 
     private void LookAtMouseDirection()
     {
+        /*
         float alpha = Mathf.Atan2(mouseWordPos.x, mouseWordPos.y);
         float alpha_in_degree = alpha / Mathf.PI;
         Quaternion curRot = transform.parent.rotation;
         curRot.z = (-1) * alpha_in_degree;
-        transform.parent.rotation = curRot;
+        transform.parent.rotation = curRot; */
+
+        this.LookAtMouseWithSmoothSpeed(rotSpeed);
+    }
+
+    private void LookAtMouseImmediately()
+    {
+        Vector2 direction = mouseWordPos - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        float offset = -90f; //because the Sprite is facing up
+        transform.parent.rotation = Quaternion.Euler(0f, 0f, angle + offset);
+    }
+
+    private void LookAtMouseWithSmoothSpeed(float rotSpeed)
+    {
+        Vector2 direction = mouseWordPos - transform.position;
+
+        float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        float offset = -90f;
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetAngle + offset);
+
+        // Rotation by time
+        transform.parent.rotation = Quaternion.RotateTowards(
+            transform.parent.rotation,
+            targetRotation,
+            rotSpeed * Time.deltaTime
+        );
     }
 }
